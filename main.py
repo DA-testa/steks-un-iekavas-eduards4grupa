ae87c39ee2ec9fe3cd5ceb341cbdc39de8653a92
eduards4grupa <125820885+eduards4grupa@users.noreply.github.com>
2023-02-19 22:39:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,11 +21,10 @@ def find_mismatch(text):
             # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
-            
             opening_brackets_stack.pop()
-        if opening_brackets_stack:
+    if opening_brackets_stack:
             return opening_brackets_stack[0].position
-        return "Success"
+    return "Success"
                 
 
 
@@ -34,8 +33,6 @@ def main():
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
     print(mismatch)
-
-if name == "main":
-    main()
+main()
 
 
