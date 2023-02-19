dec67016941b0490919c9b598a4d1e9b1f84986d
eduards4grupa <125820885+eduards4grupa@users.noreply.github.com>
2023-02-19 22:19:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if 'I' in text:
+        text = input()
     print(mismatch)
 
 main()
